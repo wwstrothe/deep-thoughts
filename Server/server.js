4925f7cc64c694a2f813b863a0ecb02fe3cb81c0
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-07-26 16:54:49 -0700

    seed to website

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
+const path = require('path');
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
-const path = require("path");
+// const path = require("path");
 
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
@@ -21,6 +22,15 @@ app.use(express.json());
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
+
+  // Serve up static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+  }
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
   db.once("open", () => {
     app.listen(PORT, () => {
