f075a90da59030a204a7325849ecddb808ddd5aa
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-08-02 22:05:14 -0700

    fixing seed

--- Server/seeders/seeds.js
+++ Server/seeders/seeds.js
@@ -1,32 +1,79 @@
-// const faker = require('faker');
-const userSeeds = require('./userSeed.json');
-const thoughtSeeds = require('./thoughtSeed.json');
-const db = require('../config/connection');
-const { Thought, User } = require('../models');
+const faker = require("faker");
 
-db.once('open', async () => {
-  try {
-    await Thought.deleteMany({});
-    await User.deleteMany({});
+const db = require("../config/connection");
+const { Thought, User } = require("../models");
 
-    await User.create(userSeeds);
+db.once("open", async () => {
+  await Thought.deleteMany({});
+  await User.deleteMany({});
 
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
-      const user = await User.findOneAndUpdate(
-        { username: thoughtAuthor },
-        {
-          $addToSet: {
-            thoughts: _id,
-          },
-        }
-      );
-    }
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
+  // create user data
+  const userData = [];
+
+  for (let i = 0; i < 50; i += 1) {
+    const username = faker.internet.userName();
+    const email = faker.internet.email(username);
+    const password = faker.internet.password();
+
+    userData.push({ username, email, password });
   }
 
-  console.log('all done!');
+  const createdUsers = await User.collection.insertMany(userData);
+
+  // create friends
+  for (let i = 0; i < 100; i += 1) {
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { _id: userId } = createdUsers.ops[randomUserIndex];
+
+    let friendId = userId;
+
+    while (friendId === userId) {
+      const randomUserIndex = Math.floor(
+        Math.random() * createdUsers.ops.length
+      );
+      friendId = createdUsers.ops[randomUserIndex];
+    }
+
+    await User.updateOne({ _id: userId }, { $addToSet: { friends: friendId } });
+  }
+
+  // create thoughts
+  let createdThoughts = [];
+  for (let i = 0; i < 100; i += 1) {
+    const thoughtText = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { username, _id: userId } = createdUsers.ops[randomUserIndex];
+
+    const createdThought = await Thought.create({ thoughtText, username });
+
+    const updatedUser = await User.updateOne(
+      { _id: userId },
+      { $push: { thoughts: createdThought._id } }
+    );
+
+    createdThoughts.push(createdThought);
+  }
+
+  // create reactions
+  for (let i = 0; i < 100; i += 1) {
+    const reactionBody = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { username } = createdUsers.ops[randomUserIndex];
+
+    const randomThoughtIndex = Math.floor(
+      Math.random() * createdThoughts.length
+    );
+    const { _id: thoughtId } = createdThoughts[randomThoughtIndex];
+
+    await Thought.updateOne(
+      { _id: thoughtId },
+      { $push: { reactions: { reactionBody, username } } },
+      { runValidators: true }
+    );
+  }
+
+  console.log("all done!");
   process.exit(0);
 });
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ThoughtList from '../components/ThoughtList';
-import FriendList from '../components/FriendList';
+import React from "react";
+import ThoughtList from "../components/ThoughtList";
+import FriendList from "../components/FriendList";
 
-import Auth from '../utils/auth';
-import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
+import Auth from "../utils/auth";
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from "../utils/queries";
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_THOUGHTS);
@@ -16,7 +16,7 @@ const Home = () => {
   return (
     <main>
       <div className="flex-row justify-space-between">
-        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
+        <div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}>
           {loading ? (
             <div>Loading...</div>
           ) : (
